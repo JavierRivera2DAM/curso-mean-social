0bef280605c6a743964e348c8a87f4dd59a1a6a2
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-20 12:42:27 +0200

    Creacion de la funcion 'saveUser' dentro de 'user.js' de controllers. Definicion de condiciones para reclamar los campos a rellenar. Definicion Variable 'bcrypt' e importacion

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,4 +1,5 @@
 'use strict'
+var bcrypt = require('bcrypt-nodejs') ;
 
 var User = require('../models/user');
 
@@ -13,6 +14,26 @@ function pruebas (req, res){
     res.status(200).send({
         message: 'Accion de pruebas en el servidor de Node'
     });
+}
+
+function saveUser(req, res){
+    var params = req.body;
+    var user = new User();
+
+    if(params.name && params.surname
+        && params.nick && params.email && params.password){
+           
+            user.name = params.name;
+            user.surname = params.surname;
+            user.nick = params.nick;
+            user.email = params.email;
+            
+    }
+    else{
+        res.status(200).send({
+            message: 'Envia todos los campos necesarios'
+        });
+    }
 }
 
 module.exports = {
