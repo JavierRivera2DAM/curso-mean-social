e1262c2b18438c0dc43d31a5502b99490532b13e
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-20 11:34:18 +0200

    Creacion Carpeta 'models' y creacion del archivo 'user.js'

---
+++ api/models/user.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema ({
+    name: String,
+    surname: String,
+    nick: String,
+    email: String,
+    password: String,
+    role: String,
+    Image: String
+});
