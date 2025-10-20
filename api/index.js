1aee69a93536fdf1bd5b68fee0bf4c09e44ae59d
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-20 08:21:17 +0200

    Modificado archivo 'index.js'. La opcion 'useMongoClient: true' ya está activada por defecto a partir de 'mongoose 5.0'

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/curso_mean_social', { useMongoClient: true})
+mongoose.connect('mongodb://localhost:27017/curso_mean_social',)
         .then(() => {
             console.log("La conexion a la base de datos curso_mean_social se ha realizado correctamente")
         })
         .catch(err => console.log(err));
+
+        //{ useMongoClient: true}
