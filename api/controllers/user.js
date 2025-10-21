7e6cfe15110d239027dee92093a1b1ae871cbb4e
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-21 13:46:24 +0200

    Creacion del metodo 'getUsers' para devolver un listado de usuarios paginado

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -119,6 +119,34 @@ async function getUser(req, res){
     }
 }
 
+//Devolver un listado de usuarios paginado
+function getUsers(req, res){
+    var identity_user_id = req.user.sub;
+
+    var page = 1;
+    if (req.params.page) {
+        page = req.params.page;        
+    }
+
+    var itemsPerPage = 5;
+
+    User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+        if(err){
+            return res.status(500).send({message: 'error en la peticion'});
+        }
+
+            if(!users){
+                return res.status(404).send({message: 'No hay usuarios disponibles'});
+            }
+        return res.status(200).send({
+            users,
+            total,
+            pages: Math.ceil(total/itemsPerPage)
+        });
+
+    });
+}
+
 
 module.exports = {
     home,
