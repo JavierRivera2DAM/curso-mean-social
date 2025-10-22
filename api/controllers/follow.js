16dc01a45a46fa9667b9fb4b12bad9c83b634ab7
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-22 15:00:21 +0200

    Creacion de la Funcion 'saveFollow' y comentarios sobre la logica que sigue al 'user'

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -6,9 +6,15 @@ var mongoosePaginate = require('mongoose-pagination');
 
 var User = require('../models/user');
 var Follow = require('../models/follow');
+const follow = require('../models/follow');
 
-function prueba(req, res){
-    res.status(200).send({message: 'Hola Mndo desde el Controlador Follows'});
+function saveFollow(req, res){
+    var params = req.body;
+
+    var follow = new Follow();
+    
+    //La propiedad 'user' del objeto 'req' adjunta un objeto con el usuario que esta logeado. Setea el user en 'authenticated.js', en el payload
+    follow.user = req.user.sub
 }
 
 module.exports = {
