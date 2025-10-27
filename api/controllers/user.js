c7f20b068b39836d2072c6967dd7013d2005f231
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-27 14:01:51 +0100

    Modificado el Metodo 'followUsersIds' para que funcione correctamente usando funcion asincrona, Antes usaba Callbacks, ahora usa 'Await'

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -232,29 +232,23 @@ async function getUsers(req, res){
 //Inicio del Punto 38
 //Creacion de una funcion asincrona llamada 'followUsersIds'
 async function followUsersIds(userId){
-    var following = await Follow.find({"user": userId}).select({'_id':0, '__v': 0, 'user': 0}).exec((err, follows) => {
-        var follows_clean = [];
+    try{
+    const following = await Follow.find({"user": userId}).select({'_id':0, '__v': 0, 'user': 0});      
 
-        follows.forEach((follow) => {
-            follows_clean.push(follow.followed);
-        });
-
-        return follows_clean;
-    });
-
-    var followed = await Follow.find({"followed": userId}).select({'_id':0, '__v': 0, 'followed': 0}).exec((err, follows) => {
-        var follows_clean = [];
-
-        follows.forEach((follow) => {
-            follows_clean.push(follow.user);
-        });
-
-        return follows_clean;
-    });
+    const followed = await Follow.find({"followed": userId}).select({'_id':0, '__v': 0, 'followed': 0});
+        
     return {
-        following: following,
-        followed: followed
+        following,
+        followed
+    };
+}
+catch(err){
+    console.error('Error en followUsersIds', err);
+    return {
+        following: [],
+        followed: []
     }
+}
 }
 
 
