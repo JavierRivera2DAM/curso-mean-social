0f6e5dd60e8505cce45694b63170a9202c2ca11d
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-28 10:53:18 +0100

    Modificacion de la Funcion 'savePublication'  para volverla asincrona

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -15,28 +15,32 @@ function probando (req, res){
     });    
 }
 
-function savePublication(req, res){
-    var params = req.body;
-
-    if(params.text){
+async function savePublication(req, res){
+    
+    const params = req.body;
+    
+    //Se realiza la Validacion de Texto
+    if(!params.text || params.text.trim() === ''){
         return res.status(200).send({message: 'Debes enviar un texto!!'});        
     }
 
-    var publication = new Publication();    
+    const publication = new Publication();    
     publication.text = params.text;
-    publication.file = 'null';
+    publication.file = null;
     publication.user = req.user.sub;
     publication.created_at = moment().unix();
 
-    publication.save((err, publicationStored) => {
-       if(err){
-        return res.status(500).send({message: 'Error al guardar la publicacion'});
-       }
+    try{
+    const publicationStored = await publication.save()       
        if(!publicationStored){
         return res.status(404).send({message: 'La publicacion NO ha sido guardada'});
        }
        return res.status(200).send({publication: publicationStored}); 
-    });
+    }
+
+catch(err){
+    return res.status(500).send({message: 'Error al guardar la publicacion', error: err.message});
+}
 }
 
 module.exports = {
