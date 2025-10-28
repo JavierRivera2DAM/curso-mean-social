faee10d2f9e00540a65919062b9d37987795c316
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-28 10:11:28 +0100

    Modificacion del archivo 'app.js' para cargar la ruta 'publication_routes'

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ var app = express();
 //Cargar Rutas
 var user_routes = require('./routes/user');
 var follow_routes = require('./routes/follow');
+var publication_routes = require('./routes/publication');
 
 //Middlewares
 app.use(bodyParser.urlencoded({extended:false}));
@@ -19,6 +20,7 @@ app.use(express.json());
 //Rutas
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
+app.use('/api', publication_routes);
 
 //Exportar
 module.exports = app;
