bbd75de3d92568823eb41f70dbaa0e2caaa1759c
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-29 09:03:23 +0100

    Inicio del Punto 45. Creacion de la Funcion 'uploadImage', se Reutiliza el codigo del Usuario para subir imagenes, y sus metodos 'removeFilesOfUploads' y 'getImageFile', Modificandolo

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -9,6 +9,7 @@ var Publication = require('../models/publication');
 var User = require('../models/user');
 var Follow = require('../models/follow');
 const publication = require('../models/publication');
+const { error } = require('console');
 
 function probando (req, res){
     res.status(200).send({
@@ -16,6 +17,7 @@ function probando (req, res){
     });    
 }
 
+//Metodo para Guardar las Publicaciones
 async function savePublication(req, res){
     
     const params = req.body;
@@ -44,6 +46,7 @@ async function savePublication(req, res){
     }
 }
 
+//Metodo para Devolver Publicaciones
 async function getPublications(req, res){
     let page = parseInt(req.params.page) || 1;
     const itemsPerPage = 4;
@@ -80,6 +83,7 @@ catch(err){
 }
 }
 
+//Metodo para Devolver una Publicacion buscando por su Id
 async function getPublication(req, res){
     try{   
     const publicationId = req.params.id;
@@ -96,6 +100,7 @@ async function getPublication(req, res){
     }
 }
 
+//Metodo para Borrar Publicaciones
 async function deletePublication(req, res){
     try{
     const publicationId = req.params.id;
@@ -112,26 +117,74 @@ async function deletePublication(req, res){
     }
 }
 
+//Metodo para subir fichero en la publicacion
+async function uploadImage(req, res){
+    
+    var publicationId = req.params.id;    
 
-// function deletePublication(req, res){
-//     var publicationId = req.params.id;
+    // if(publicationId != req.user.sub){
+    //     return res.status(403).send({ message: 'No tienes permiso para actualizar los datos de la publicación', error: err.message });
+    // }
+        if(!req.files || !req.files.image){
+            return res.status(400).send({ message: 'No se han subido imagenes', error: err.message});
+        }
 
-//     Publication.findByIdAndRemove(publicationId, (err, publicationRemoved) => {
-//         if(err){
-//             return res.status(500).send({message: 'Error al borrar publicaciones'});            
-//         }
-//         if(!publication){
-//             return res.status(404).send({message: 'No existe la publicacion'});
-//         }
-//         return res.status(200).send({publication: publicationRemoved});
-//     });
-// }
+        var file_path = req.files.image.path;
+        var file_name = require('path').basename(file_path);
+        var file_ext = require('path').extname(file_name).toLowerCase().replace('.', '');
+        var validExtensions = ['png', 'jpg', 'jpeg', 'gif'];                
+
+        if(!validExtensions.includes(file_ext)){
+           return removeFilesOfUploads(res, file_path, 'Extension no valida', err.message);
+        }
+
+        try{
+            //Actualizar documento de publicacion
+            var publicationUpdated = await Publication.findByIdAndUpdate(publicationId, { image: file_name }, { new: true});
+                
+                if(!publicationUpdated){
+                    return res.status(404).send({message: 'No se ha podido actualizar', error: err.message});
+                }
+                return res.status(200).send({publication: publicationUpdated, message: 'La Imagen ha sido Subida CORRECTAMENTE'});
+            }
+         catch(err){
+            return res.status(500).send({message: 'Error en la peticion', error: err.message});
+        }                            
+}
+
+//Método para borrar un Archivo de Imagen Subido
+function removeFilesOfUploads(res, file_path, message){
+    fs.unlink(file_path, (err) => {
+        if (err) {
+        return res.status(500).send({message: 'Error al eliminar el archivo'});
+    }
+        return res.status(200).send({message: message});
+    });
+}
+
+//Devolver Imagen de Usuario
+function getImageFile(req, res){
+    var image_file = req.params.imageFile;    
+    var path_file = path.resolve('./uploads/publications/', image_file);
+
+    fs.access(path_file, fs.constants.F_OK, (err) => {
+        if (err){
+            res.status(200).send({message: 'No existe la imagen...'});            
+        }
+            res.sendFile(path.resolve(path_file));
+            if(err){
+                return res.status(500).send({ message: 'Error al enviar la imagen'});
+            }       
+    });
+}
 
 module.exports = {
     probando,
     savePublication,
     getPublications,
     getPublication,
-    deletePublication
+    deletePublication,
+    uploadImage,
+    getImageFile
 
 }
