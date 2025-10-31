889d9e65e69d39e29af8f9ceb28bf2e85ba87038
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-31 14:47:20 +0100

    Correcciones Realizadas para Confirmar el Uso de Standalone en el proyecto. Sigue sin aparecer el Mensaje de '<login>'

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 
 //Agregado 'standalone: false' al Componente para evitar errores al usar NgModules
 @Component({    
   selector: 'app-root', 
-  imports: [CommonModule, RouterModule], 
+  imports: [CommonModule, RouterModule, LoginComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,15 +1,14 @@
 import  {Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({        
     selector: 'login',
+    imports: [CommonModule],
     templateUrl: './login.component.html'
 })
-export class LoginComponent implements OnInit{
-    public title:string;
-
-    constructor(){
-        this.title= 'Identificate';
-    }
+export class LoginComponent {
+    public title:string= 'Identificate';
+    
 
     ngOnInit() {
         console.log ('Componente de login cargado...');
