22c642b10be8ec9ada00c20864d4641d89b4e19c
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-10-31 13:27:49 +0100

    Creado archivo 'app.modules.ts'

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+//Componentes
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    imports: [
+        BrowserModule
+    ], providers: [],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule{}
