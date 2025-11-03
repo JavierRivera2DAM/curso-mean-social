9bd81b7be456676c16c1da59c06acfa337ff6d98
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-11-03 09:34:27 +0100

    Modificaciones para Eliminar Errores

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { NgModule } from "@angular/core";
+//import { NgModule } from "@angular/core";
 
 //Componentes
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { BrowserModule } from '@angular/platform-browser';
-//import { NgModule } from '@angular/core';
-//import { routing, appRoutingProviders } from './app.routing';
+import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders } from './app.routing';
 
 @NgModule({
     declarations: [    
@@ -18,9 +18,9 @@ import { BrowserModule } from '@angular/platform-browser';
         AppComponent,
         LoginComponent,
         RegisterComponent,
-        //routing         
+        routing         
     ], providers: [
-        //appRoutingProviders
+        appRoutingProviders
     ],
     bootstrap: [AppComponent]
 })
--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+//import { routing } from './app/app.routing';
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)]
