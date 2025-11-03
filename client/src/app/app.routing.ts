6af7e94c98a65ee31a8b82c4052ed489cc4553fc
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-11-03 09:03:50 +0100

    Añadido Codigo 'app.routing.module.ts'. Se importan los modulos y los componentes login y register. Definicion Constantes y Exportar Configuracion

---
+++ client/src/app/app.routing.ts
@@ -0,0 +1,18 @@
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+//Importar Componentes
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+
+//Definicion de Constantes
+const appRoutes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'registro', component: RegisterComponent},
+
+];
+
+//Exportacion de la Configuracion
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
