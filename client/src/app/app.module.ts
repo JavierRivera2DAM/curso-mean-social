7cad82440c7ff8e40747f907832af95424453b27
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-11-03 09:15:41 +0100

    Se han eliminado los imports de 'NgModule', 'routing' y 'appRoutingProviders' dentro de 'app.module.ts'

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { BrowserModule } from '@angular/platform-browser';
+//import { NgModule } from '@angular/core';
+//import { routing, appRoutingProviders } from './app.routing';
 
 @NgModule({
     declarations: [    
@@ -15,8 +17,11 @@ import { BrowserModule } from '@angular/platform-browser';
         BrowserModule,
         AppComponent,
         LoginComponent,
-        RegisterComponent         
-    ], providers: [],
+        RegisterComponent,
+        //routing         
+    ], providers: [
+        //appRoutingProviders
+    ],
     bootstrap: [AppComponent]
 })
 
