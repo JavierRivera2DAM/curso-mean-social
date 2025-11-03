fe52266ede0bb5b64c3b0534237716f9e7cd855a
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-11-03 08:26:34 +0100

    Modificacion del 'app.component.html' para incluir '<register></register>'. Dio un error inicial, porque no se habia importado dentro de los 'imports' de 'app.component.ts'. Fin Punto 71

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 //Agregado 'standalone: false' al Componente para evitar errores al usar NgModules
 @Component({    
   selector: 'app-root', 
-  imports: [CommonModule, RouterModule, LoginComponent], 
+  imports: [CommonModule, RouterModule, LoginComponent, RegisterComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
