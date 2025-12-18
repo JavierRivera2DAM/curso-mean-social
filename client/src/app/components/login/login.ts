e6ede44e7943ae1771983f8e2f8f91d157e611fc
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-12-18 09:10:06 +0100

    Creado el Componente de Login en el Front

---
+++ client/src/app/components/login/login.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'login',
+    templateUrl: './login.html'
+})
+
+export class LoginComponent implements OnInit{
+    public title:string;
+
+        constructor(){
+            this.title = 'Identificate';
+        }
+
+
+    ngOnInit(): void {
+        console.log('Componente de login cargado...');
+    }
+}
