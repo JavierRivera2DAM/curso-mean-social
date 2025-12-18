2bd2b28fed15610a5e9d25323a5718b58028a4cd
JavierRivera2DAM <jrivouv096@g.educaand.es>
2025-12-18 14:00:29 +0100

    Meodificación del Método Registro para que muestre Mesnaje de 'Campo Obligatorio' al tocar sobre un campo y no escribir nada en el 'register component'

--- client/src/app/components/register/register.ts
+++ client/src/app/components/register/register.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { User } from "../../models/user";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
 
 @Component({
     selector: 'app-register',
     standalone: true,
+    imports: [FormsModule, CommonModule],
     templateUrl: './register.html'
 })
 
 export class RegisterComponent implements OnInit{
     public title:string;
+    public user: User;
 
-        constructor(){
+        constructor(
+            private _route: ActivatedRoute,
+            private _router: Router
+        ){  
             this.title = 'Regístrate';
+            this.user = new User("",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "ROLE_USER",
+                ""
+            );          
         }
 
 
     ngOnInit(): void {
         console.log('Componente de register cargado...');
+    }
+
+    //Método invocado desde el Template
+    onSubmit(form:any): void {
+        console.log('Formulario enviado:', form.value);
     }
 }
